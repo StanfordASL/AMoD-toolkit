c9843ed772cf2b07cff627df57eb2fe2c4047a0b
Alvaro Estandia <alvaro@estandia.com>
2018-08-26 16:17:25 -0500

    Added more comments to aaSampleUseCase

--- aaSampleUseCase.m
+++ aaSampleUseCase.m
@@ -3,20 +3,25 @@
 clear variables;
 
 %% Prepare specification
+% Load demo EAMoDspec
 data = load('EAMoDspecDemo.mat');
 spec = data.spec;
 
+% Add random charger electricity prices in [35,45] USD per MWh
 % Seed for repeatability
 rng('default');
-% Add random charger electricity prices in [35,45] USD per MWh
 charger_electricity_price_usd_per_j = (35 + 10*rand(spec.n_charger,spec.n_time_step))/(1e6*3600);
 spec.charger_electricity_price_usd_per_j = charger_electricity_price_usd_per_j;
 
-%% Create and solve problem
+%% Explore solution to EAMoDproblem in the traditional formulation
+
+% Instantiate EAMoDproblem based on spec
 eamod_problem = EAMoDproblem(spec);
 
-[objective_value,solver_time] = eamod_problem.Solve();
+% Solve optimization problem
+[objective_value,solver_time_s] = eamod_problem.Solve();
 
+% Evaluate the different cost components in the optimal solution
 [amod_cost_usd,pax_cost_usd,reb_cost_usd] = eamod_problem.EvaluateAMoDcost();
 electricity_cost_usd = eamod_problem.EvaluateElectricityCost();
 
@@ -25,28 +30,21 @@ eamod_problem.PlotRoadGraph();
 eamod_problem.PlotVehicleState();
 eamod_problem.PlotDeparturesAndArrivals();
 
-%% Compare with real-time formulation
+%% Explore solution to EAMoDproblem in the real-time formulation
+
+% Instantiate EAMoDproblemRT based on spec
 eamod_problem_rt = EAMoDproblemRT(spec);
 
-[objective_value_rt,solver_time_rt] = eamod_problem_rt.Solve();
+[objective_value_rt,solver_time_s_rt] = eamod_problem_rt.Solve();
 
+% Evaluate the different cost components in the optimal solution
 [amod_cost_usd_rt,pax_cost_usd_rt,reb_cost_usd_rt] = eamod_problem_rt.EvaluateAMoDcost();
-
 electricity_cost_usd_rt = eamod_problem_rt.EvaluateElectricityCost();
 
+% Explore relative difference in amod_cost and solver_time_s for traditional and real-time formulations
 amod_cost_rel_difference = (amod_cost_usd_rt - amod_cost_usd)/amod_cost_usd
-solver_time_rel_difference = (solver_time_rt - solver_time)/solver_time
+solver_time_s_rel_difference = (solver_time_s_rt - solver_time_s)/solver_time_s
 
 %% Plot results
 eamod_problem_rt.PlotVehicleState();
 eamod_problem_rt.PlotDeparturesAndArrivals();
-
-
-
-
-
-
-
-
-
-
